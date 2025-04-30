95ca517e2a79500399e6f2328d52d55d7c7ee944
Joachim <joachim.dev@pm.me>
2025-04-30 03:49:58 +0200

    use neovim-nightly

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,14 +10,14 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux neovim neovide
+dnf5 install -y tmux neovide
 
 # Use a COPR Example:
 #
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
+dnf5 -y copr enable agriffis/neovim-nightly
+dnf5 -y install neovim
 # Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr disable agriffis/neovim-nightly
 
 #### Example for enabling a System Unit File
 
