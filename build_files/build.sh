5dbb184690e06006a67df6f172ddd1268178a966
Jo <119578282+Charging1948@users.noreply.github.com>
2025-05-01 21:55:28 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux neovide stow go uv
+dnf5 install -y tmux stow go uv
 
 # Use a COPR Example:
 #
