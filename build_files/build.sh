0488b00ecc1c4be546255244df12e71424490cef
Jo <119578282+Charging1948@users.noreply.github.com>
2025-05-10 17:43:07 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,7 +11,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs packages from fedora repos
-dnf5 install -y tmux stow go uv podman-docker podman-compose podman-tui podlet plasma-browser-integration
+dnf5 install -y tmux stow go uv podman-docker podman-compose podman-tui podlet plasma-browser-integration dnf-plugins-core
 
 # Use a COPR Example:
 #
@@ -22,6 +22,11 @@ dnf5 -y copr disable lukenukem/asus-linux
 systemctl enable supergfxd.service
 
 dnf5 install -y asusctl-rog-gui
+
+dnf5 -y copr enable codifryed/CoolerControl
+dnf5 install -y coolercontrol liquidctl
+dnf5 -y copr disable codifryed/CoolerControl
+systemctl enable coolercontrold
 
 # Use a COPR Example:
 #
