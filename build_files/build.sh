0034f96fa82316d7ab8b405e52fe668b185742fe
Jo <joachim.dev@pm.me>
2025-05-13 12:52:17 +0200

    remove artifacts of custom kernel stuff, fixup distrobox.ini

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,15 +2,6 @@
 
 set -ouex pipefail
 
-dnf5 -y copr enable bieszczaders/kernel-cachyos
-dnf5 install -y kernel-cachyos kernel-cachyos-devel-matched
-dnf5 -y copr disable bieszczaders/kernel-cachyos
-
-dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
-dnf5 install -y libcap-ng libcap-ng-devel procps-ng procps-ng-devel
-dnf5 install -y uksmd
-dnf5 -y copr disable bieszczaders/kernel-cachyos-addons
-
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -19,7 +10,7 @@ dnf5 -y copr disable bieszczaders/kernel-cachyos-addons
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs packages from fedora repos
-dnf5 install -y tmux stow go uv podman-docker podman-compose podman-tui podlet plasma-browser-integration dnf-plugins-core
+dnf5 install -y podman-docker podman-compose podman-tui podlet plasma-browser-integration dnf-plugins-core
 
 # Use a COPR Example:
 #
@@ -30,18 +21,6 @@ dnf5 -y copr disable lukenukem/asus-linux
 systemctl enable supergfxd.service
 
 dnf5 install -y asusctl-rog-gui
-
-dnf5 -y copr enable codifryed/CoolerControl
-dnf5 install -y coolercontrol liquidctl
-dnf5 -y copr disable codifryed/CoolerControl
-systemctl enable coolercontrold
-
-# Use a COPR Example:
-#
-dnf5 -y copr enable agriffis/neovim-nightly
-dnf5 install -y neovim
-# Disable COPRs so they don't end up enabled on the final image:
-dnf5 -y copr disable agriffis/neovim-nightly
 
 #### Example for enabling a System Unit File
 
